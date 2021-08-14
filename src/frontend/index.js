444197c63793ebc8054de50c086325b3abb83f11
Steve Berardi <sberardi@gmail.com>
2021-08-14 13:50:23 -0700

    result page header

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -3,11 +3,38 @@ import ReactDOM from "react-dom";
 
 require('purecss');
 
-class Brew extends React.Component {
-  render() {
-    return (<div style="visible: none;">Hello {this.props.name}</div>);
-  }
+function Brew () {
+  return (
+    <div className="navbar pure-g">
+      <div className="pure-u-1-6">
+        <p className="navbar-header"><img className="pure-img navbar-logo" src="images/wordbrew.png"/> WordBrew</p>
+      </div>
+      <div className="pure-u-2-3">
+          
+
+          <form className="pure-form">
+            <fieldset>
+              <input type="text" className="pure-input-2-3 pure-input-rounded" />
+              <button className="pure-button button-brew">Brew!</button>
+            </fieldset>
+          </form>
+
+      </div>
+    </div>
+
+    
+  );
 }
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<Brew name="wordzz" />, mountNode);
+function App() {
+  return (
+    <div>
+      <Brew name="wordzz" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+);
