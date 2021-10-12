dc62dc8a9fa88c5f7ee8bbb5026b51b8c4bcf6e3
Steve Berardi <sberardi@gmail.com>
2021-10-11 20:52:20 -0700

    basic test layout

---
+++ backend/test_all.py
@@ -0,0 +1,17 @@
+from wordbrew import brew
+
+
+def test_brew():
+    results = brew("walk")
+
+    # TODO: assert valid results
+
+    assert results
+
+
+def test_server_valid_query():
+    pass
+
+
+def test_server_bad_query():
+    pass
