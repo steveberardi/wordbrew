ddd8c1294e805cc7ed2421b09b3b94d905979cad
Steve Berardi <sberardi@gmail.com>
2021-10-12 21:17:23 -0700

    add assertion for brew

--- backend/test_all.py
+++ backend/test_all.py
@@ -1,12 +1,26 @@
 from wordbrew import brew
 
 
-def test_brew():
+def test_brew_with_real_word():
     results = brew("walk")
+    expected_keys = set(
+        [
+            "id",
+            "pos",
+            "weight",
+            "definition",
+            "hypernyms",
+            "hyponyms",
+            "lemmas",
+            "similar",
+        ]
+    )
+    for r in results:
+        assert set(r.keys()) - expected_keys == set()
 
-    # TODO: assert valid results
 
-    assert results
+def test_brew_with_fake_word():
+    assert brew("foobar") == []
 
 
 def test_server_valid_query():
