83d2aefe8e3ae301ac8f949bab577e8fec472f2f
Steve Berardi <sberardi@gmail.com>
2021-11-07 12:36:11 -0800

    Frontend updates (#24)
    * fix footer bg color
    
    * no margin on foooter
    
    * no bottom margin on no results alert
    
    * use buildkit

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -7,7 +7,7 @@ import { Row, Col } from 'react-bootstrap';
 export const Footer = () => {
 
     return (
-        <footer className="py-5 mt-2 bg-light bg-opacity-50">
+        <footer className="py-5 bg-light">
             <Container>
                 <Row>
                     <Col md={6}>
