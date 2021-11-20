fc49b8085e74c64e84b4f8ec406cb12a30e972f8
Steve Berardi <sberardi@gmail.com>
2021-11-19 16:14:50 -0800

    keep accordion items open

--- frontend/src/Brew.js
+++ frontend/src/Brew.js
@@ -59,13 +59,6 @@ export const Brew = () => {
       )
   }, [query]);
 
-  function handleSenseClick(e) {
-    e.target.scrollIntoView({
-      block: "end",
-      behavior: "smooth"
-    });
-  }
-
   return (
     <div>
       <Navbar bg="light" variant="light" expand="md" className="pb-2">
@@ -102,10 +95,10 @@ export const Brew = () => {
             <Col md={2}></Col>
             
             <Col className="justify-content-center">
-              <Accordion defaultActiveKey="0">
-                {results.length > 0 ?
-                  results.map( (result, index) => (
-                    <Accordion.Item eventKey={index.toString()} key={index} onClick={handleSenseClick}>
+              {results.length > 0 ?
+                results.map( (result, index) => (
+                  <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey={index.toString()} key={index} >
 
                       <Accordion.Header>
                         <span className="fs-2 fw-bold">{query}</span>
@@ -132,22 +125,22 @@ export const Brew = () => {
                         }
                       </Accordion.Body>
 
-                    </Accordion.Item>  
-                  ))
-                  :
-                  <div className="alert alert-dark text-center m-0">
-                    <br/><br/>
+                    </Accordion.Item>
+                  </Accordion>
+                ))
+                :
+                <div className="alert alert-dark text-center m-0">
+                  <br/><br/>
 
-                    <svg width="90" viewBox="0 0 25 25" fill="#684808" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-.019 14c1.842.005 3.613.791 5.117 2.224l-.663.748c-1.323-1.27-2.866-1.968-4.456-1.972h-.013c-1.568 0-3.092.677-4.4 1.914l-.664-.748c1.491-1.4 3.243-2.166 5.064-2.166h.015zm-3.494-6.5c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm7.013 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"/></svg>
+                  <svg width="90" viewBox="0 0 25 25" fill="#684808" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm-.019 14c1.842.005 3.613.791 5.117 2.224l-.663.748c-1.323-1.27-2.866-1.968-4.456-1.972h-.013c-1.568 0-3.092.677-4.4 1.914l-.664-.748c1.491-1.4 3.243-2.166 5.064-2.166h.015zm-3.494-6.5c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm7.013 0c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1z"/></svg>
 
-                    <br/><br/>
-                    
-                    <p className="fs-4 lead">No results found, keep brewing!</p>
+                  <br/><br/>
 
-                    <br/><br/>
-                  </div>
-                }
-              </Accordion>
+                  <p className="fs-4 lead">No results found, keep brewing!</p>
+
+                  <br/><br/>
+                </div>
+              }
             </Col>
 
             <Col md={2}></Col>
